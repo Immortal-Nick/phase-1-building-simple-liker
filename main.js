6e059d7523996d4f3e71a3d9d0d4ce5b94031db2
Nixon <kiplangatnixon28@gmail.com>
2024-07-17 00:38:49 +0300

    creates a liker

--- main.js
+++ main.js
@@ -4,7 +4,33 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  const hearts = document.querySelectorAll('.like-glyph');
+  const modal = document.getElementById('modal');
+  modal.classList.add('hidden');
 
+  hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.textContent === EMPTY_HEART) {
+            heart.textContent = FULL_HEART;
+            heart.classList.add('activated-heart');
+          } else {
+            heart.textContent = EMPTY_HEART;
+            heart.classList.remove('activated-heart');
+          }
+        })
+        .catch(error => {
+          modal.classList.remove('hidden');
+          modal.querySelector('#modal-message').textContent = error;
+          setTimeout(() => {
+            modal.classList.add('hidden');
+          }, 3000);
+        });
+    });
+  });
+});
 
 
 //------------------------------------------------------------------------------
